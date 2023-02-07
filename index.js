4787d6971ad877ea1ca5e81ab57e60a819872099
Štěpán Prchal <prchalek@gmail.com>
2023-02-07 20:07:01 +0100

    miluju arrays s ovocem

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const pole = [];
 
-pole.push("banan", "jablko", "hruska");
+console.log(pole);
+pole.push("banan", "jablko", "hruska"); // prida prvky na konec pole !! konec pole
+
+console.log(pole.push("mandarinka")) // vrati delku pole po pridani prvku
+
+console.log(pole);
 console.log(`mazu ${pole.pop()}`) // odstrani posledni prvek pole a vrati ho
 
 console.log(pole);
-pole.delete(1); // odstrani prvek na indexu 1
-console.log(pole);
+
+const kdeBANAN = pole.indexOf("banan"); // notable, ze arrays zacinaj indexem 0
+console.log(kdeBANAN); // 0
+
+const coNAINDEXU1 = pole[1]; // druhy prvek pole
+console.log(coNAINDEXU1); // jablko
