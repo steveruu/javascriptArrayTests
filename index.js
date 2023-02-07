79a215d42ea722216474ea1711e1cae1e9d39c7a
Štěpán Prchal <prchalek@gmail.com>
2023-02-07 19:59:43 +0100

    что ??????

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ pole.push("banan", "jablko", "hruska");
 console.log(`mazu ${pole.pop()}`) // odstrani posledni prvek pole a vrati ho
 
 console.log(pole);
+pole.delete(1); // odstrani prvek na indexu 1
+console.log(pole);
